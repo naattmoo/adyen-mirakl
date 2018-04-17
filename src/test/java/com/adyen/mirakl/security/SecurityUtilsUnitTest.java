b06768f09ae2dc2f429d9ceb663f87a92226dd24
Rik ter Beek <rik.terbeek@adyen.com>
2018-04-17 15:47:21 +0200

    reverted some removed code that is usefull, and reverted some usefull tests

---
+++ src/test/java/com/adyen/mirakl/security/SecurityUtilsUnitTest.java
@@ -0,0 +1,27 @@
+package com.adyen.mirakl.security;
+
+import org.junit.Test;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Test class for the SecurityUtils utility class.
+ *
+ * @see SecurityUtils
+ */
+public class SecurityUtilsUnitTest {
+
+    @Test
+    public void testgetCurrentUserLogin() {
+        SecurityContext securityContext = SecurityContextHolder.createEmptyContext();
+        securityContext.setAuthentication(new UsernamePasswordAuthenticationToken("admin", "admin"));
+        SecurityContextHolder.setContext(securityContext);
+        Optional<String> login = SecurityUtils.getCurrentUserLogin();
+        assertThat(login).contains("admin");
+    }
+}
