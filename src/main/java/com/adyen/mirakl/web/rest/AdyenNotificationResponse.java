9276c1ed75bfb3e9460b78c74193a01b9fc6a342
Giorgos Adam <georgios.adam@adyen.com>
2018-11-08 13:33:57 +0100

    Modified Adyen notifications response to contain "[accepted]"

---
+++ src/main/java/com/adyen/mirakl/web/rest/AdyenNotificationResponse.java
@@ -0,0 +1,38 @@
+/*
+ *                       ######
+ *                       ######
+ * ############    ####( ######  #####. ######  ############   ############
+ * #############  #####( ######  #####. ######  #############  #############
+ *        ######  #####( ######  #####. ######  #####  ######  #####  ######
+ * ###### ######  #####( ######  #####. ######  #####  #####   #####  ######
+ * ###### ######  #####( ######  #####. ######  #####          #####  ######
+ * #############  #############  #############  #############  #####  ######
+ *  ############   ############  #############   ############  #####  ######
+ *                                      ######
+ *                               #############
+ *                               ############
+ *
+ * Adyen Mirakl Connector
+ *
+ * Copyright (c) 2018 Adyen B.V.
+ * This file is open source and available under the MIT license.
+ * See the LICENSE file for more info.
+ *
+ */
+package com.adyen.mirakl.web.rest;
+
+public class AdyenNotificationResponse {
+    public String notificationResponse;
+
+    public AdyenNotificationResponse() {
+        notificationResponse = "[accepted]";
+    }
+
+    public String getNotificationResponse() {
+        return notificationResponse;
+    }
+
+    public void setNotificationResponse(String notificationResponse) {
+        this.notificationResponse = notificationResponse;
+    }
+}
